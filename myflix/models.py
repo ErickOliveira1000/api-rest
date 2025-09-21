2e30d74474c3d97543440f1ae5dc3178aa5afda4
Erick1000 <erick.oliveira@pditabira.com>
2025-09-21 18:00:09 -0300

    Adiciona migrations e atualiza models

--- myflix/models.py
+++ myflix/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    name = models.CharField(max_length = 100)
+class User(models.Model):
+    nome = models.CharField(max_length = 100)
     email = models.EmailField(blank=False, max_length = 30)
     cpf = models.CharField(max_length = 11)
-    data_nascimento = models.DataField()
+    data_nascimento = models.DateField()
     celular = models.CharField(max_length = 14)
 
     def __str__(self):
         return self.nome
 
 
-class stream(models.Model):
+class Stream(models.Model):
     CATEGORIA = (
     ('F', 'Filme'),
     ('S', 'Série'),
     ('D', 'Documentário')
     )
-    codigo = models.CharField(max_lengt=10)
-    descricao = models.CharField(max_lengt=100, blank=False)
-    categoria = models.CharField(max_length=1, choices=CATEGORIA, blank=False, defalte='F')
+    codigo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100, blank=False)
+    categoria = models.CharField(max_length=1, choices=CATEGORIA, blank=False, default='F')
 
     def __str__(self):
         return self.codigo
