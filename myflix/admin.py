1a56f2ebfea6524a7ae602fd7cf7863e5e306ebb
Erick1000 <erick.oliveira@pditabira.com>
2025-09-29 21:21:37 -0300

    models

--- myflix/admin.py
+++ myflix/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from myflix.models import User, Stream
 
 # Register your models here.
+
+class Users(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_display_links = ('id', 'nome')
+    list_per_page = 20
+    search_fields = ['nome']
+
+
+
+class Streams(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links = ('id', 'codigo')
+    search_fields = ['codigo']
+
+
+admin.site.register(User, Users)
+admin.site.register(Stream, Streams)
